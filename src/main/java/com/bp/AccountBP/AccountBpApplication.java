a01d20daca6436a8d7ba973e045a6db2a6aa6fc9
Galo Ortega <galoefren@hotmail.com>
2024-07-11 22:49:05 -0500

    Base project

---
+++ src/main/java/com/bp/AccountBP/AccountBpApplication.java
@@ -0,0 +1,13 @@
+package com.bp.AccountBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bp/AccountBP/AccountBpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bp.AccountBP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
